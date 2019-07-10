020e215a60b8eea1e73b12663976c480164a2a53
Ivan Zhang <z243155476@gmail.com>
2019-07-09 20:17:06 -0700

    Config change

--- api/app.js
+++ api/app.js
@@ -41,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use("/messages", messagesRouter);
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
